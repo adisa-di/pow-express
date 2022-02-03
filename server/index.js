f247448083c692b0901680d2d50565fc22cdac36
Adisa Narula <adisan19@gmail.com>
2022-02-03 22:44:37 +0700

    added client scripting

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const Miner = require('./src/mine');
 const bodyParser = require('body-parser');
-const { generateKeys } = require('./src/util');
+const { generateKeys, signMessage, verifyMessage } = require('./src/util');
 const { balances } = require('./src/db');
+const db = require('./src/db');
 
 const app = express();
 const miner = new Miner(1500); // 500 ms
@@ -35,6 +36,33 @@ app.get('/generate/:amount', (req, res) => {
   addressAndBalances.forEach(add => accountInfos +=  `Account: ${add.address}, Balance: ${add.balance}\n`);
 
   res.send(accountInfos);
+});
+
+/**
+ * These functions will usually exist on the client side
+ * But for the purpose of this exercise, we're putting it here
+ */
+
+// message, private key
+app.post('/signMessage', (req, res) => {
+  // sign the message with the private key
+  const { message } = req.body;
+
+  const { from } = message;
+  const private = db.balances[from].privateKey;
+
+  const msgStr = JSON.stringify(message);
+  const signed = signMessage(private, msgStr);
+  res.send(signed);
+});
+
+app.post('/verifyMessage', (req, res) => {
+  const { message, signature } = req.body;
+
+  const { from } = JSON.parse(message);
+  const publicKey = db.balances[from].publicKey;
+  const verify = verifyMessage(signature, publicKey, message);
+  res.send(verify);
 });
 
 app.post('/transaction', (req, res) => {
